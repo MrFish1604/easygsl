49daf9245f0d1564bff63742f5447cfadceb4f2e
MrFish1604 <mmrfishc@gmail.com>
2021-12-10 19:22:54 +0100

    Create a simple Vector class

---
+++ include/gslppvector.hpp
@@ -0,0 +1,37 @@
+#ifndef GSLPPVECTOR
+#define GSLPPVECTOR
+
+#include <iostream>
+#include <gsl/gsl_vector.h>
+
+class Vector
+{
+public:
+    Vector(unsigned int n);
+    Vector(unsigned int n, const double x);
+    double getne(const unsigned int& i);
+    void setne(const unsigned int& i, const double value);
+    std::string toString();
+    void zeros();
+    void setAll(const double x);
+    void basisne(const unsigned int i);
+    void operator+=(const Vector& v);
+    void operator*=(const Vector& v);
+    void operator*=(const double& a);
+    void operator/=(const Vector& v);
+    void operator/=(const double& a);
+private:
+    gsl_vector* _vect;
+    unsigned int _size;
+};
+
+Vector operator+(const Vector& a, const Vector& b);
+Vector operator*(const Vector& a, const Vector& b);
+Vector operator*(const Vector& v, const double& a);
+Vector operator*(const double& a, const Vector& v);
+Vector operator/(const Vector& a, const Vector& b);
+Vector operator/(const Vector& v, const double& a);
+
+std::ostream& operator<<(std::ostream& stream, Vector& vect);
+
+#endif
---
+++ src/gslppvector.cpp
@@ -0,0 +1,117 @@
+#include "gslppvector.hpp"
+
+Vector::Vector(const unsigned int n): _size(n)
+{
+    _vect = gsl_vector_alloc(_size);
+}
+Vector::Vector(unsigned int n, const double x): _size(n)
+{
+    _vect = gsl_vector_alloc(_size);
+    setAll(x);
+}
+
+double Vector::getne(const unsigned int& i)
+{
+    return gsl_vector_get(_vect, i);
+}
+
+void Vector::setne(const unsigned int& i, const double value)
+{
+    gsl_vector_set(_vect, i, value);
+}
+
+std::string Vector::toString()
+{
+    std::string str = "(";
+    str += std::to_string(getne(0));
+    for(int i=1; i<_size; i++)
+    {
+        str += "  ";
+        str += std::to_string(getne(i));
+    }
+    str += ")";
+    return str;
+}
+
+void Vector::zeros()
+{
+    gsl_vector_set_zero(_vect);
+}
+
+void Vector::setAll(const double x)
+{
+    gsl_vector_set_all(_vect, x);
+}
+
+void Vector::basisne(const unsigned int i)
+{
+    gsl_vector_set_basis(_vect, i);
+}
+
+void Vector::operator+=(const Vector& v)
+{
+    gsl_vector_add(_vect, v._vect);
+}
+
+void Vector::operator*=(const Vector& v)
+{
+    gsl_vector_mul(_vect, v._vect);
+}
+
+void Vector::operator*=(const double& a)
+{
+    gsl_vector_scale(_vect, a);
+}
+
+void Vector::operator/=(const Vector& v)
+{
+    gsl_vector_div(_vect, v._vect);
+}
+
+void Vector::operator/=(const double& a)
+{
+    gsl_vector_scale(_vect, 1/a);
+}
+
+Vector operator+(const Vector& a, const Vector& b)
+{
+    Vector c(a);
+    c+=b;
+    return c;
+}
+Vector operator*(const Vector& a, const Vector& b)
+{
+    Vector c(a);
+    c*=b;
+    return c;
+}
+Vector operator*(const Vector& v, const double& a)
+{
+    Vector c(v);
+    c*=a;
+    return c;
+}
+Vector operator*(const double& a, const Vector& v)
+{
+    Vector c(v);
+    c*=a;
+    return c;
+}
+Vector operator/(const Vector& a, const Vector& b)
+{
+    Vector c(a);
+    c/=b;
+    return c;
+}
+Vector operator/(const Vector& v, const double& a)
+{
+    Vector c(v);
+    c/=a;
+    return c;
+}
+
+std::ostream& operator<<(std::ostream& stream, Vector& vect)
+{
+    stream << vect.toString();
+    return stream;
+}
