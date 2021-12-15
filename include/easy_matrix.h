3828e0a0d1c2345cff9746dab85f043b8d6508e9
MrFish1604 <mmrfishc@gmail.com>
2021-12-15 20:58:45 +0100

    Improve Matrix class not tested

--- include/easy_matrix.h
+++ include/easy_matrix.h
@@ -2,16 +2,28 @@
 #define EASYMATRIX
 
 #include <gsl/gsl_matrix.h>
+#include <iostream>
+
+typedef struct couple couple;
+struct couple
+{
+    unsigned int x;
+    unsigned int y;
+};
 
 class Matrix
 {
 public:
-    Matrix(const unsigned int n, const unsigned int m);
+    Matrix(const unsigned int& n, const unsigned int& m);
+    Matrix(const unsigned int& n, const unsigned int& m, const double value);
     Matrix(const Matrix& mat);
+    double& at(const unsigned int& i, const unsigned int& j);
+    std::string toString();     // TODO
     void setIdentity();
+    double& operator[](const couple& i);
 private:
     unsigned int _nLines, _nCols;
-    gsl_matrix _matrix;
+    gsl_matrix* _matrix;
 };
 
 Matrix createIndentity(const unsigned int n, const unsigned int m);
--- src/easy_matrix.cpp
+++ src/easy_matrix.cpp
@@ -1,10 +1,14 @@
 #include "easy_matrix.h"
 
-Matrix::Matrix(const unsigned int n, const unsigned int m) _nLines(n), _nCols(m)
+Matrix::Matrix(const unsigned int& n, const unsigned int& m): _nLines(n), _nCols(m)
 {
     _matrix = gsl_matrix_alloc(_nLines, _nCols);
 }
-
+Matrix::Matrix(const unsigned int& n, const unsigned int& m, const double value): _nLines(n), _nCols(m)
+{
+    _matrix = gsl_matrix_alloc(_nLines, _nCols);
+    gsl_matrix_set_all(_matrix, value);
+}
 Matrix::Matrix(const Matrix& mat)
 {
     _nLines = mat._nLines;
@@ -13,10 +17,30 @@ Matrix::Matrix(const Matrix& mat)
     gsl_matrix_memcpy(_matrix, mat._matrix);
 }
 
+double& Matrix::at(const unsigned int& i, const unsigned int& j)
+{
+    if(i>=_nLines || j>=_nCols)
+        throw std::out_of_range("Index out of range");
+    else
+        return *gsl_matrix_ptr(_matrix, i, j);
+}
+
+std::string Matrix::toString()
+{
+    return "";
+}
+
+double& Matrix::operator[](const couple& i)
+{
+    return *gsl_matrix_ptr(_matrix, i.x, i.y);
+}
+
 void Matrix::setIdentity()
 {
     gsl_matrix_set_identity(_matrix);
 }
+
+
 
 Matrix createIndentity(const unsigned int n, const unsigned int m)
 {
