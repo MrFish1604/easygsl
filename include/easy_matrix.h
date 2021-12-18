863308e066eb28b0cd6af7d07270a9b8709b00d8
MrFish1604 <mmrfishc@gmail.com>
2021-12-18 13:18:45 +0100

    Add Matrix operators

--- include/easy_matrix.h
+++ include/easy_matrix.h
@@ -3,12 +3,13 @@
 
 #include <gsl/gsl_matrix.h>
 #include <iostream>
+#include <gsl/gsl_blas.h>
 
 typedef struct couple couple;
 struct couple
 {
-    unsigned int x;
-    unsigned int y;
+    int x;
+    int y;
 };
 
 class Matrix
@@ -18,14 +19,38 @@ public:
     Matrix(const unsigned int& n, const unsigned int& m, const double value);
     Matrix(const Matrix& mat);
     double& at(const unsigned int& i, const unsigned int& j);
+    double& atne(const unsigned int& i, const unsigned int& j);
+    int nrows();
+    int nlines();
     std::string toString();     // TODO
     void setIdentity();
+    void operator+=(const Matrix& mat);
+    void operator+=(const double& a);
+    void operator-=(const Matrix& mat);
+    void operator-=(const double& a);
+    void operator*=(const Matrix& mat);
+    void operator*=(const double& a);
+    void operator/=(const Matrix& mat);
+    void operator/=(const double& a);
     double& operator[](const couple& i);
+    ~Matrix();
 private:
     unsigned int _nLines, _nCols;
     gsl_matrix* _matrix;
+
+friend Matrix dot(const Matrix& A, const Matrix& B);
 };
 
 Matrix createIndentity(const unsigned int n, const unsigned int m);
+
+Matrix operator+(const Matrix& mat, const Matrix& mat1);
+Matrix operator+(const Matrix& mat, const double& a);
+Matrix operator-(const Matrix& mat, const Matrix& mat1);
+Matrix operator-(const Matrix& mat, const double& a);
+Matrix operator*(const Matrix& mat, const Matrix& mat1);
+Matrix operator*(const Matrix& mat, const double& a);
+Matrix operator/(const Matrix& mat, const Matrix& mat1);
+Matrix operator/(const Matrix& mat, const double& a);
+std::ostream& operator<<(std::ostream& stream, Matrix& mat);
 
 #endif
--- src/easy_matrix.cpp
+++ src/easy_matrix.cpp
@@ -25,9 +25,67 @@ double& Matrix::at(const unsigned int& i, const unsigned int& j)
         return *gsl_matrix_ptr(_matrix, i, j);
 }
 
+double& Matrix::atne(const unsigned int& i, const unsigned int& j)
+{
+    return *gsl_matrix_ptr(_matrix, i, j);
+}
+
+int Matrix::nrows(){ return _nCols; }
+
+int Matrix::nlines(){ return _nLines; }
+
 std::string Matrix::toString()
 {
-    return "";
+    std::string rtn = "|" + std::to_string(gsl_matrix_get(_matrix, 0, 0));
+    for(int i=1; i<_nCols; i++)
+        rtn += "\t" + std::to_string(gsl_matrix_get(_matrix, 0, i));
+    rtn += "|";
+    for(int i=1; i<_nLines; i++)
+    {
+        rtn += "\n|" + std::to_string(gsl_matrix_get(_matrix, i, 0));
+        for(int j=1; j<_nCols; j++)
+        {
+            rtn += "\t" + std::to_string(gsl_matrix_get(_matrix, i, j));
+        }
+        rtn += "|";
+    }
+    return rtn;
+}
+
+void Matrix::operator+=(const Matrix& mat)
+{
+    gsl_matrix_add(_matrix, mat._matrix);
+}
+void Matrix::operator+=(const double& a)
+{
+    gsl_matrix_add_constant(_matrix, a);
+}
+
+void Matrix::operator-=(const Matrix& mat)
+{
+    gsl_matrix_sub(_matrix, mat._matrix);
+}
+void Matrix::operator-=(const double& a)
+{
+    gsl_matrix_add_constant(_matrix, -a);
+}
+
+void Matrix::operator*=(const Matrix& mat)
+{
+    gsl_matrix_mul_elements(_matrix, mat._matrix);
+}
+void Matrix::operator*=(const double& a)
+{
+    gsl_matrix_scale(_matrix, a);
+}
+
+void Matrix::operator/=(const Matrix& mat)
+{
+    gsl_matrix_div_elements(_matrix, mat._matrix);
+}
+void Matrix::operator/=(const double& a)
+{
+    gsl_matrix_scale(_matrix, 1/a);
 }
 
 double& Matrix::operator[](const couple& i)
@@ -40,6 +98,10 @@ void Matrix::setIdentity()
     gsl_matrix_set_identity(_matrix);
 }
 
+Matrix::~Matrix()
+{
+    gsl_matrix_free(_matrix);
+}
 
 
 Matrix createIndentity(const unsigned int n, const unsigned int m)
@@ -47,4 +109,66 @@ Matrix createIndentity(const unsigned int n, const unsigned int m)
     Matrix mat(n, m);
     mat.setIdentity();
     return mat;
+}
+
+Matrix operator+(const Matrix& mat, const Matrix& mat1)
+{
+    Matrix c(mat);
+    c+=mat1;
+    return c;
+}
+Matrix operator+(const Matrix& mat, const double& a)
+{
+    Matrix c(mat);
+    c+= a;
+    return c;
+}
+Matrix operator-(const Matrix& mat, const Matrix& mat1)
+{
+    Matrix c(mat);
+    c-= mat1;
+    return c;
+}
+Matrix operator-(const Matrix& mat, const double& a)
+{
+    Matrix c(mat);
+    c-= a;
+    return c;
+}
+Matrix operator*(const Matrix& mat, const Matrix& mat1)
+{
+    Matrix c(mat);
+    c*= mat1;
+    return c;
+}
+Matrix operator*(const Matrix& mat, const double& a)
+{
+    Matrix c(mat);
+    c*= a;
+    return c;
+}
+Matrix operator/(const Matrix& mat, const Matrix& mat1)
+{
+    Matrix c(mat);
+    c/= mat1;
+    return c;
+}
+Matrix operator/(const Matrix& mat, const double& a)
+{
+    Matrix c(mat);
+    c/= a;
+    return c;
+}
+
+std::ostream& operator<<(std::ostream& stream, Matrix& mat)
+{
+    stream << mat.toString();
+    return stream;
+}
+
+Matrix dot(const Matrix& A, const Matrix& B)
+{
+    Matrix C(A._nLines, B._nCols);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1., A._matrix, B._matrix, 0., C._matrix);
+    return C;
 }
